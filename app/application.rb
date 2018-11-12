b95a4d2f6cb814772f133abbd3fdf9f4e16405f5
Douglas Uretsky <douglasuretsky@gmail.com>
2018-11-12 21:34:18 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,5 @@
 class Application
    def call(env)
-    resp = Rack::Response.new
      if Time.now.hour.between?(0, 11)
       Rack::Response.new.write "Good Morning!"
     elsif Time.now.hour.between?(12, 17)

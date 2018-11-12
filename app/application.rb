8c21353b7c4f0a99450dea7e42fabeac8fbc4298
Douglas Uretsky <douglasuretsky@gmail.com>
2018-11-12 21:33:33 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,10 +1,12 @@
-class Application
-  def call(env)
-    hour = Time.new.hour
-    if hour < 12
-      Rack::Response.new.write "Good Morning!"
+def call(env)
+    resp = Rack::Response.new
+     if Time.now.hour.between?(0, 11)
+      resp.write "Good Morning!"
+    elsif Time.now.hour.between?(12, 17)
+      resp.write "Good Afternoon."
     else
-      Rack::Response.new.write "Good Afternoon!"
+      resp.write "Good Evening!"
     end
+     resp.finish
   end
 end

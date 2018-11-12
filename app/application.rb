dfa47d7f9fb1d02b4bb332e16d2d1548500fb97f
Douglas Uretsky <douglasuretsky@gmail.com>
2018-11-12 21:26:39 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,11 +1,3 @@
 class Application
-  def call
-   hour = Time.new.hour
-   resp = Rack::Response.new
-   if hour < 12
-     resp.write "Good Morning!"
-   else
-     resp.write "Good Afternoon!"
-   end
- end
+  
 end
